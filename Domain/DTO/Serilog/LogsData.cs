f6e3d1784c965f30fca80142f83fb6b72419bf17
Mohammad Abu Haieh <m.abuhaieh@e-incube.com>
2025-07-08 15:02:13 +0300

    edit file name

---
+++ Domain/DTO/Serilog/LogsData.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.DTO.Logs
+{
+    public class LogsData
+    {
+        public int Id { get; set; }
+
+        public string Timestamp { get; set; } = string.Empty;
+
+        public string Level { get; set; } = string.Empty;
+
+        public string? Exception { get; set; }
+
+        public string? RenderedMessage { get; set; }
+
+        public string? Properties { get; set; }
+    }
+}
---
+++ Domain/DTO/Serilog/LogsFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTO.Logs
+{
+    public class LogsFilter
+    {
+        public string Keyword { get; set; }
+        public int PageSize { get; set; }
+        public int PageIndex { get; set; }
+    }
+}
